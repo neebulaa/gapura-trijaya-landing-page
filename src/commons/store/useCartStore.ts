9000a903722441e2f97238cc71d28a6a0e72dec9
murprakoso <prakoso.clan04@gmail.com>
2024-06-03 09:43:17 +0700

    fix: remove implicity warning in useCartStore. add build chunk size warning limit in vite config

--- src/commons/store/useCartStore.ts
+++ src/commons/store/useCartStore.ts
@@ -1,3 +1,56 @@
+// import { create } from 'zustand';
+// import { persist } from 'zustand/middleware';
+
+// interface CartState {
+//   items: any[];
+//   addItem: (item: any) => void;
+//   removeItem: (id: number) => void;
+// }
+
+// const useCartStore = create<CartState>()(
+//   persist(
+//     (set) => ({
+//       items: [],
+//       addItem: (item) => set({ items: [...items, item] }),
+//       removeItem: (id) =>
+//         set({
+//           items: items.filter((item) => item.id !== id),
+//         }),
+//     }),
+//     {
+//       name: 'cart-storage',
+//       storage: {
+//         getItem: (name) => {
+//           const str = localStorage.getItem(name);
+//           if (!str) return null;
+//           const { state, version } = JSON.parse(str);
+//           return {
+//             state: {
+//               ...state,
+//               transactions: new Map(state.transactions),
+//             },
+//             version,
+//           };
+//         },
+//         setItem: (name, newValue: any) => {
+//           const str = JSON.stringify({
+//             state: {
+//               ...newValue,
+//               transactions: Array.from(newValue.transactions.entries()),
+//             },
+//             version: newValue.version,
+//           });
+//           localStorage.setItem(name, str);
+//         },
+//         removeItem: (name) => localStorage.removeItem(name),
+//       },
+//     }
+//   )
+// );
+
+// export default useCartStore;
+
+// ## 
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
@@ -11,11 +64,14 @@ const useCartStore = create<CartState>()(
   persist(
     (set) => ({
       items: [],
-      addItem: (item) => set({ items: [...items, item] }),
+      addItem: (item) =>
+        set((state) => ({
+          items: [...state.items, item],
+        })),
       removeItem: (id) =>
-        set({
-          items: items.filter((item) => item.id !== id),
-        }),
+        set((state) => ({
+          items: state.items.filter((item) => item.id !== id),
+        })),
     }),
     {
       name: 'cart-storage',
@@ -25,19 +81,13 @@ const useCartStore = create<CartState>()(
           if (!str) return null;
           const { state, version } = JSON.parse(str);
           return {
-            state: {
-              ...state,
-              transactions: new Map(state.transactions),
-            },
+            state,
             version,
           };
         },
-        setItem: (name, newValue: any) => {
+        setItem: (name, newValue) => {
           const str = JSON.stringify({
-            state: {
-              ...newValue,
-              transactions: Array.from(newValue.transactions.entries()),
-            },
+            state: newValue.state,
             version: newValue.version,
           });
           localStorage.setItem(name, str);
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Set batas menjadi 1000 kB (1 MB), Default: 500 kB
+  },
 });
