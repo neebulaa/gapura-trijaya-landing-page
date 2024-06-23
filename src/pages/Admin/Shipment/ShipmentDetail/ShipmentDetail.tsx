432bedf12b8792596c1a4eecf829215e77af1e7c
murprakoso <prakoso.clan04@gmail.com>
2024-06-23 23:29:47 +0900

    Add shipment detail

--- src/pages/Admin/Shipment/ShipmentDetail/ShipmentDetail.tsx
+++ src/pages/Admin/Shipment/ShipmentDetail/ShipmentDetail.tsx
@@ -1,10 +1,18 @@
 import usePageEffect from '@/commons/hooks/usePageEffect';
+import { separator } from '@/commons/utils/Currency/Currency';
 import useShipmentDetailController from '@/pages/Admin/Shipment/ShipmentDetail/ShipmentDetailController';
-import { Breadcrumb, Card } from 'antd';
+import { Breadcrumb, Card, Col, List, Row, Skeleton, Space, Table, Tag } from 'antd';
+import dayjs from 'dayjs';
 
 export default function ShipmentDetail() {
   /** Controller */
-  const { breadcrumbItem } = useShipmentDetailController();
+  const {
+    breadcrumbItem,
+    shipmentData,
+    shipmentDataIsFetching,
+    shipmentDataIsRefetch,
+    OrderItemsTableProps,
+  } = useShipmentDetailController();
 
   usePageEffect({
     index: false,
@@ -15,8 +23,108 @@ export default function ShipmentDetail() {
   return (
     <>
       <Breadcrumb items={breadcrumbItem} />
-      <Card>
-        <div>Shipment Detail</div>
+      <Card title={`Shipment Detail`}>
+        {shipmentDataIsFetching ? (
+          <Skeleton />
+        ) : (
+          <>
+            <Row gutter={24} className="mb-4">
+              <Col xl={8} lg={8}>
+                <p className="text-dark mb-2 text-primary uppercase font-medium text-base">
+                  Billing Address
+                </p>
+                <address>
+                  {shipmentData?.data?.order?.customerFirstName} {shipmentData?.data?.order?.customerLastName}
+                  <br /> {shipmentData?.data?.order?.customerAddress1}
+                  <br /> {shipmentData?.data?.order?.customerAddress2}
+                  <br />
+                  <span className="font-semibold">Email:</span> {shipmentData?.data?.order?.customerEmail}
+                  <br />
+                  <span className="font-semibold">Phone:</span> {shipmentData?.data?.order?.customerPhone}
+                  <br />
+                  <span className="font-semibold">Postcode:</span>{' '}
+                  {shipmentData?.data?.order?.customerPostcode}
+                </address>
+              </Col>
+              <Col xl={8} lg={8}>
+                <p className="text-dark mb-2 text-primary uppercase font-semibold text-base">
+                  Shipment Address
+                </p>
+                <address>
+                  {shipmentData?.data?.order?.customerFullName}
+                  <br /> {shipmentData?.data?.order?.customerAddress1}
+                  <br /> {shipmentData?.data?.order?.customerAddress2}
+                  <br />
+                  <span className="font-semibold">Email:</span> {shipmentData?.data?.order?.customerEmail}
+                  <br />
+                  <span className="font-semibold">Phone:</span> {shipmentData?.data?.order?.customerPhone}
+                  <br />
+                  <span className="font-semibold">Postcode:</span>{' '}
+                  {shipmentData?.data?.order?.customerPostcode}
+                </address>
+              </Col>
+              <Col xl={8} lg={8}>
+                <p className="text-dark mb-2 text-primary uppercase font-medium text-base">
+                  Details
+                </p>
+                <address>
+                  <span className="font-semibold">Invoice ID: </span>
+                  <span className="font-semibold text-blue">#{shipmentData?.data?.order?.code}</span>
+                  <br /> {dayjs(shipmentData?.data?.order?.orderDate).format('HH:mm:ss DD/MM/YYYY')}
+                  <br /> <span className="font-semibold">Status:</span>
+                  <Tag className="ml-2">{shipmentData?.data?.order?.status}</Tag>
+                  <br /> <span className="font-semibold">Payment Status:</span>
+                  <Tag color="yellow" className="ml-2">
+                    {shipmentData?.data?.order?.paymentStatus}
+                  </Tag>
+                  <br /> <span className="font-semibold">Shipped by:</span>{' '}
+                  <span className="font-semibold text-blue">
+                    {shipmentData?.data?.order?.shippingServiceName}
+                  </span>
+                </address>
+              </Col>
+            </Row>
+            <Row gutter={24}>
+              <Col span={24}>
+                <Table
+                  rowKey={(record) => record.id!}
+                  scroll={{ x: 700 }}
+                  bordered={true}
+                  size="small"
+                  columns={OrderItemsTableProps}
+                  dataSource={shipmentData?.data?.order?.orderItems}
+                  pagination={false}
+                />
+              </Col>
+            </Row>
+            <Row gutter={24} className="mt-4">
+              <Col span={8} offset={16}>
+                <List className="cart-page-total">
+                  <List.Item>
+                    <Space>
+                      Subtotal <span>Rp {separator(shipmentData?.data?.order?.baseTotalPrice)}</span>
+                    </Space>
+                  </List.Item>
+                  {/* <List.Item>
+                    <Space>
+                      Tax (10%) <span>Rp {separator(shipmentData?.data?.order?.taxAmount)}</span>
+                    </Space>
+                  </List.Item> */}
+                  <List.Item>
+                    <Space>
+                      Shipping Cost <span>Rp {separator(shipmentData?.data?.order?.shippingCost)}</span>
+                    </Space>
+                  </List.Item>
+                  <List.Item className="font-semibold">
+                    <Space>
+                      Total <span>Rp {separator(shipmentData?.data?.order?.grandTotal)}</span>
+                    </Space>
+                  </List.Item>
+                </List>
+              </Col>
+            </Row>
+          </>
+        )}
       </Card>
     </>
   );
--- src/pages/Admin/Shipment/ShipmentDetail/ShipmentDetailController.tsx
+++ src/pages/Admin/Shipment/ShipmentDetail/ShipmentDetailController.tsx
@@ -1,4 +1,8 @@
-import { Link } from "react-router-dom";
+import { separator } from '@/commons/utils/Currency/Currency';
+import { useGetShipment } from '@/services/queries/admin/shipment.query';
+import { IOrderItem } from '@/types/order';
+import { ColumnType } from 'antd/es/table';
+import { Link, useParams } from 'react-router-dom';
 
 export default function useShipmentDetailController() {
   /**
@@ -10,7 +14,75 @@ export default function useShipmentDetailController() {
     { title: `Detail` },
   ];
 
+  const { id } = useParams();
+
+  /**
+   * Query Model Get One Order
+   */
+  const {
+    data: shipmentData,
+    isPending: shipmentDataIsFetching,
+    refetch: shipmentDataIsRefetch,
+  } = useGetShipment(id!, { enabled: true });
+
+  /**
+   * Order Items Table Columns Props
+   */
+  const OrderItemsTableProps: ColumnType<IOrderItem>[] = [
+    {
+      title: 'SKU',
+      dataIndex: 'sku',
+      key: 'sku',
+    },
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Attributes',
+      dataIndex: 'attributes',
+      key: 'attributes',
+      render: (text: string) => {
+        if (!text) return 'No description available';
+        let attributes;
+        try {
+          attributes = JSON.parse(text);
+        } catch (error) {
+          return 'Invalid description format';
+        }
+        if (!attributes || typeof attributes !== 'object') return 'No description available';
+        const attributeItems = Object.entries(attributes).map(
+          ([key, value]) => `<li>${key.charAt(0).toUpperCase() + key.slice(1)}: ${value}</li>`,
+        );
+        return <div dangerouslySetInnerHTML={{ __html: `<ul>${attributeItems.join('')}</ul>` }} />;
+      },
+    },
+    {
+      title: 'Qty',
+      dataIndex: 'qty',
+      key: 'qty',
+      align: 'center',
+    },
+    {
+      title: 'Base Price',
+      dataIndex: 'basePrice',
+      key: 'basePrice',
+      render: (text) => `Rp ${separator(text)}`,
+    },
+    {
+      title: 'Sub Total',
+      dataIndex: 'subTotal',
+      key: 'subTotal',
+      render: (text) => `Rp ${separator(text)}`,
+    },
+  ];
+
   return {
     breadcrumbItem,
+    shipmentData,
+    shipmentDataIsFetching,
+    shipmentDataIsRefetch,
+    OrderItemsTableProps,
   };
 }
